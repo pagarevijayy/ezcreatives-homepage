2427373cd5e85a9b49ac713d8b4529b0a9fb7029
pagarevijayy <pagarevj97@gmail.com>
2021-06-13 02:34:42 +0530

    added templates: wisdom +2

--- pages/image-builder/templates.js
+++ pages/image-builder/templates.js
@@ -1,4 +1,9 @@
-import { Wisdom0, Wisdom1 } from "../../lib/design-templates/classic-designs";
+import {
+  Wisdom0,
+  Wisdom1,
+  Wisdom2,
+  Wisdom3,
+} from "../../lib/design-templates/classic-designs";
 import Container from "../../layouts/container";
 
 const templates = () => {
@@ -19,13 +24,24 @@ const templates = () => {
             author="Bertrand Russell"
             imageURL="https://www.the-philosophy.com/wp-content/uploads/2011/06/bertrand_russell_image.jpg"
             socialHandle="wethesapiens_"
-          ></Wisdom0>
+          />
           <Wisdom1
             quote="You know, there are two good things in life, freedom of thought and freedom of action."
             author="Bertrand Russell"
             imageURL="https://www.the-philosophy.com/wp-content/uploads/2011/06/bertrand_russell_image.jpg"
             socialHandle="The Philosophy Quote"
             mainContentPadding="px-8"
+          />
+          <Wisdom2
+            quote="The less people know, the more stubbornly they know it."
+            author="Osho"
+            imageURL="https://pbs.twimg.com/media/E3tVluFVoAcLfpI.jpg"
+          />
+          <Wisdom3
+            quote="If you want to be great, walk the rope without a boss and without a net."
+            author="Naval"
+            imageURL="https://pbs.twimg.com/profile_images/1256841238298292232/ycqwaMI2_400x400.jpg"
+            mainContentPadding="px-0"
           />
         </div>
       </Container>
